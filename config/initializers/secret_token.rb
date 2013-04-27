7030f23fb9d8b58d7a02be4cef0574448506ca7e
Tanaka Kazuki <yakuruto771@live.jp>
2013-04-27 20:07:21 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zisui::Application.config.secret_token = 'e0e8ea7434a26d8dfe65f954528325df89f3bdd4c2ea3b7c0e7463ddc48a7d70bc3b7fd94c0ee35f1a88c81331222714c87589290a1c04c3259e54ed45741dfc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Zisui::Application.config.session_store :cookie_store, key: '_zisui_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Zisui::Application.config.session_store :active_record_store
